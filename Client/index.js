0822ad0e2f6d5b7e445dbae05f9d72a5df73d79e
Andrew <yya123@sfu.ca>
2019-02-06 21:46:21 -0800

    fixed a small bug, updated readme and screenshots, now server is permanantly deployed at heroku

--- Client/index.js
+++ Client/index.js
@@ -9,6 +9,8 @@ const sys = require("util");
 const win = require("node-windows");
 
 const port = 8000;
+const LOCAL_URL = "http://localhost:3000/synchronization";
+const HEROKU_URL = "https://blooming-falls-51922.herokuapp.com/synchronization";
 
 // app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
 
@@ -21,7 +23,7 @@ app.get("/synchronization", (req, res) => {
   let timeBefore = new Date().getTime();
   console.log("time before: " + timeBefore);
   axios
-    .get("http://localhost:3000/synchronization")
+    .get(HEROKU_URL)
     .then(response => {
       // calculate RTT = Time after sending HTTP request - time before sending HTTP request
       let timeAfter = new Date();
@@ -30,7 +32,7 @@ app.get("/synchronization", (req, res) => {
       let serverTime = new Date(response.data);
 
       // calculate the new time = Server time + RTT / 2;
-      let newTime = new Date(timeAfter.getTime() + RTT / 2);
+      let newTime = new Date(serverTime.getTime() + RTT / 2);
 
       // get formatted date and time string to set system time
       let day = newTime.getDate();
